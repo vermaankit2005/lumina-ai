55f48e9253910155e74e761d73be93a8ffeada3c
Ankit Verma <vermaankit.2005@gmail.com>
2026-04-16 22:41:22 +0200

    feat: add spring boot application skeleton

---
+++ src/main/java/com/luminaai/LuminaAiApplication.java
@@ -0,0 +1,11 @@
+package com.luminaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuminaAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LuminaAiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/luminaai/LuminaAiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.luminaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaAiApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
